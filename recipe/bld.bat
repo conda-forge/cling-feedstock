f5882a95274efa3fd66b164931238a0cf07ea32c
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-01-31 17:24:44 +0100

    Attempt nmake build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,15 @@
 mkdir build
 cd build
 
-cmake -G "Ninja" ^
+cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DLIBCLING_BUILD_STATIC=ON ^
       %SRC_DIR%
-
 if errorlevel 1 exit 1
 
-ninja -j%CPU_COUNT%
+nmake
 if errorlevel 1 exit 1
 
-ninja install
+nmake install
 if errorlevel 1 exit 1
